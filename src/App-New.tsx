e262a177c7ef2d2b65a15be82bf2ade788751be5
abhijitGuragain <gabhijit@insightworkshop.io>
2024-12-31 14:52:42 +0545

    my first commit

--- src/App-New.tsx
+++ src/App-New.tsx
@@ -1,38 +1,39 @@
 import "./App.css";
+import "react-dom/client";
 import ZoomMtgEmbedded from "@zoom/meetingsdk/embedded";
 
 function App() {
   const client = ZoomMtgEmbedded.createClient();
 
-  const authEndpoint = ""; // http://localhost:4000
-  const sdkKey = "";
-  const meetingNumber = "";
-  const passWord = "";
-  const role = 0;
-  const userName = "React";
-  const userEmail = "";
-  const registrantToken = "";
-  const zakToken = "";
+  // const authEndpoint = ""; // http://localhost:4000
+  const sdkKey = "SDK";
+  const meetingNumber = "meeting number";
+  const passWord = "meeting password";
+  // const role = 1;
+  const userName = "username";
+  const userEmail = "user email";
+  // const registrantToken = "";
+  const zakToken = "zak token if any";
 
-  const getSignature = async () => {
-    try {
-      const req = await fetch(authEndpoint, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          meetingNumber: meetingNumber,
-          role: role,
-        }),
-      });
-      const res = await req.json()
-      const signature = res.signature as string;
-      startMeeting(signature)
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const getSignature = async () => {
+  //   try {
+  //     const req = await fetch(authEndpoint, {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         meetingNumber: meetingNumber,
+  //         role: role,
+  //       }),
+  //     });
+  //     const res = await req.json()
+  //     const signature = res.signature as string;
+  //     startMeeting(signature)
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
-  async function startMeeting(signature: string) {
+  async function startMeeting() {
     const meetingSDKElement = document.getElementById("meetingSDKElement")!;
     try {
       await client.init({
@@ -40,17 +41,18 @@ function App() {
         language: "en-US",
         patchJsMedia: true,
         leaveOnPageUnload: true,
-      })
+      });
       await client.join({
-        signature: signature,
+        signature: `Your generated signature
+`,
         sdkKey: sdkKey,
         meetingNumber: meetingNumber,
         password: passWord,
         userName: userName,
         userEmail: userEmail,
-        tk: registrantToken,
+        // tk: registrantToken,
         zak: zakToken,
-      })
+      });
       console.log("joined successfully");
     } catch (error) {
       console.log(error);
@@ -65,7 +67,7 @@ function App() {
         <div id="meetingSDKElement">
           {/* Zoom Meeting SDK Component View Rendered Here */}
         </div>
-        <button onClick={getSignature}>Join Meeting</button>
+        <button onClick={startMeeting}>Join Meeting</button>
       </main>
     </div>
   );
